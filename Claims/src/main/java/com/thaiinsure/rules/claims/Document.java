2925c561c627be1ed294a732231e8fedc4876b41
brmsAdmin <>
2017-02-20 16:41:29 +0700

    {/Claims/src/main/java/com/thaiinsure/rules/claims/Document.java}

--- Claims/src/main/java/com/thaiinsure/rules/claims/Document.java
+++ Claims/src/main/java/com/thaiinsure/rules/claims/Document.java
@@ -9,6 +9,7 @@ public class Document implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   @org.kie.api.definition.type.Description("e.g. prescription or bill")
    private java.lang.String documentType;
    private java.util.Date createDate;
    private java.lang.String documentDescription;
